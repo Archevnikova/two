12e84ceac9e342ffae11d298ad00d4c7d1277254
Lenovo Radeon <ms.orlovakatya@mail.ru>
2023-09-28 14:07:34 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,8 @@
+let button=document.querySelector('.i-button');
+let rectanglee=document.querySelector('.rectanglee');        
+
+button.onclick=function (){
+    rectanglee.classList.toggle('hidden');
+    
+};
+  
